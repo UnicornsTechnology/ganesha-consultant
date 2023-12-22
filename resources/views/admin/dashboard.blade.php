0116046124a77f4828153c24a09de50065b7e648
SWapnil Mane <swapnilm.ajspire@gmail.com>
2023-12-22 23:58:36 +0530

    CARDS ROW REMOVED

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -63,65 +63,6 @@
     </div>
     <!--end row-->
 
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-xl-4 row-cols-xxl-4">
-        <div class="col">
-            <div class="card radius-10 border-0 border-start border-primary border-4">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div class="">
-                            <p class="mb-1">Plans Purchased Grooms</p>
-                        </div>
-                        <div class="ms-auto widget-icon bg-primary text-white">
-                            <i class="bi bi-basket2-fill"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card radius-10 border-0 border-start border-success border-4">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div class="">
-                            <p class="mb-1">Plans Purchased Brides</p>
-                        </div>
-                        <div class="ms-auto widget-icon bg-success text-white">
-                            <i class="bi bi-currency-dollar"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card radius-10 border-0 border-start border-danger border-4">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div class="">
-                            <p class="mb-1">Monthly Income</p>
-                        </div>
-                        <div class="ms-auto widget-icon bg-danger text-white">
-                            <i class="bi bi-graph-down-arrow"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card radius-10 border-0 border-start border-warning border-4">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div class="">
-                            <p class="mb-1">Total Income</p>
-                        </div>
-                        <div class="ms-auto widget-icon bg-warning text-dark">
-                            <i class="bi bi-people-fill"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="card">
         <div class="card-body">
             <h2>Recently Purchased Plans Members</h2>
